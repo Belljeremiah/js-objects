5629ee09e4d0f95703a41bb4f48734f49d26c031
Jeremiah Bell <jeremiah.bell812.topper@gmail.com>
2019-10-09 13:42:29 -0600

    Created first set of lighting exercises in main.js 1-3

---
+++ src/scripts/main.js
@@ -0,0 +1,43 @@
+console.log("We is LEArnInag aboUT ObJeCTs!?")
+
+
+const muscleCar = {
+    carColor : "red",
+    carYear : "2015", 
+    carModel : "Mustang", 
+    carManufacturer : "Ford", 
+}
+
+console.log("Muscle Car", muscleCar);
+
+const animalNames = ["Kippers", "Jack", "Gypsy", "Angus", "Seymour Bouts", "Sharky"];
+
+const parentOne = {
+    name: "Bob",
+    gender: "non-binary",
+    age: 82
+
+}
+
+const parentTwo = {
+    name: "Chad",
+    gender: "Male",
+
+}
+
+const parentThree = {
+    name: "Madonna",
+    gender: "Madonna",
+    age: 257
+
+}
+
+const child = {
+    name: "Child",
+    gender: "Undecided",
+    age: 64
+}
+
+const familyMembers = [parentOne, parentTwo, parentThree, child]
+
+console.log("Family Members", familyMembers)
